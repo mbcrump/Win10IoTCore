16b4074058c39727f35e0533bbc8c29c43b791cc
mbcrump <mbcrump29@gmail.com>
2015-07-02 16:24:56 -0500

    Initial Commit

---
+++ Win10IoTCore/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Win10IoTCore
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Data> data = new List<Data>();
+            data.Add(new Data() { Category = "Apples", Value = 5 });
+            data.Add(new Data() { Category = "Oranges", Value = 9 });
+            data.Add(new Data() { Category = "Pineaples", Value = 8 });
+
+            this.barSeries.DataContext = data;
+        }
+    }
+}
